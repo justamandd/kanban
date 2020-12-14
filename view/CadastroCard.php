52d2dfdd6b0b147a729a0a0d1d36296ff56cfa74
Lucas2003-18 <70024244+Lucas2003-18@users.noreply.github.com>
2020-12-14 20:11:42 -0300

    Cadastro de Card's

---
+++ view/CadastroCard.php
@@ -0,0 +1,21 @@
+<?php
+    ob_start();
+?>
+
+</head>
+    <body>
+        <div class="container">
+            <div class="centered">
+            <!-- Form de Cadastro de Card -->
+                <form action="" method="POST" name="cardCad" id="cardCad">
+
+                    <div class="card-header">
+                        <h1 class="card-title">Cadastro de Card</h1>
+                    </div>
+
+                <!-- 
+
+                </form>
+            </div>
+        </div>
+    </body>
